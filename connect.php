dc8975116a1a60d4f84d3b089ea0f87ac82b2ca5
Chaz Zyl G. Llaguno <chazzyl.llaguno@gmail.com>
2024-05-08 07:01:15 +0800

    changed dbname, & updated report!

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php 
-	$connection = new mysqli('localhost', 'root','','dbjaranillaf3');
+	$connection = new mysqli('localhost', 'root','','dbloudwavemusic');
 	
 	if (!$connection){
 		die (mysqli_error($mysqli));
