5c6af2c8c59016b7758086d2531bf150475c5616
Chaz Zyl G. Llaguno <chazzyl.llaguno@gmail.com>
2024-05-31 23:10:08 +0800

    contact

--- contactus.php
+++ contactus.php
@@ -2,12 +2,12 @@
     include 'includes/header.php';
 ?>
  
-<div class="grid2container">
+<div class="grid2container" style="color: white">
     <div class="rightgrid">
-        agfwasg
+        <h1> Phone Number: 092-4312-434</h1>
     </div>
 
     <div class="leftgrid">
-    agsg
+    <h1> Email: LoudWaveMusic@lwd.com</h1>
     </div>
 </div>
